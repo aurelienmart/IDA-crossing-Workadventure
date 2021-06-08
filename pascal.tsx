8e48ded788e0866f455d8ef5c83a619410e7aebf
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-06-08 17:54:48 +0200

    Delete pascal.tsx

--- pascal.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="pascal" tilewidth="0" tileheight="0" tilecount="0" columns="2">
- <grid orientation="orthogonal" width="10" height="3"/>
-</tileset>
