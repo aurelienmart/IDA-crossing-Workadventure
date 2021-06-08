d979db6da9867f7f4e757b57a913303e54dd4e70
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-06-08 17:39:02 +0200

    static pascal

--- pascal.tsx
+++ pascal.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="pascal" tilewidth="54" tileheight="61" tilecount="4" columns="1">
- <grid orientation="orthogonal" width="1" height="1"/>
+<tileset version="1.5" tiledversion="1.7.0" name="pascal" tilewidth="54" tileheight="61" tilecount="4" columns="2">
+ <grid orientation="orthogonal" width="10" height="3"/>
  <tile id="9">
   <image width="48" height="57" source="../../../Desktop/dos.png"/>
   <objectgroup draworder="index" id="2">
