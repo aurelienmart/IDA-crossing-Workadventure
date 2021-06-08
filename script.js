4696e6b047faf70bd39985d0763aba2e9dc2c47c
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-06-08 14:54:46 +0200

    test popup

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-WA.sendChatMessage('Bienvenue au 17eme étage, pensez à autoriser les pop-ups depuis ce site et à activer le son afin de profiter de toutes les fonctionnalités.','La team IDA Crossing')
+WA.sendChatMessage('Bienvenue au 17eme étage, pensez à autoriser les pop-ups depuis ce site et à activer le son afin de profiter de toutes les fonctionnalités.','La team IDA Crossing');
+
+openPopup(targetObject: "Popup", message: "test", buttons:  [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+}]);
