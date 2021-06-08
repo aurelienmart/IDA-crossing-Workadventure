4dcdf1b7bbcd65a154098f56eff0b745953f7eb9
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-06-08 17:45:58 +0200

    test static pascal

--- pascal.tsx
+++ pascal.tsx
@@ -2,24 +2,36 @@
 <tileset version="1.5" tiledversion="1.7.0" name="pascal" tilewidth="54" tileheight="61" tilecount="4" columns="2">
  <grid orientation="orthogonal" width="10" height="3"/>
  <tile id="9">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
   <image width="48" height="57" source="../../../Desktop/dos.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="4" y="0" width="43" height="57"/>
   </objectgroup>
  </tile>
  <tile id="10">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
   <image width="54" height="60" source="../../../Desktop/face.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="4" y="0" width="48" height="57"/>
   </objectgroup>
  </tile>
  <tile id="11">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
   <image width="48" height="61" source="../../../Desktop/profilD.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="4" y="2" width="42" height="57"/>
   </objectgroup>
  </tile>
  <tile id="12">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
   <image width="48" height="60" source="../../../Desktop/ProfilG.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="3" y="0" width="41" height="57"/>
