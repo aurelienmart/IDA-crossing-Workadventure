627d48e93a89fd675b699a1811a664b66a36df92
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-06-08 17:28:24 +0200

    add static pascal

---
+++ pascal.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="pascal" tilewidth="54" tileheight="61" tilecount="4" columns="1">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="9">
+  <image width="48" height="57" source="../../../Desktop/dos.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="43" height="57"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="54" height="60" source="../../../Desktop/face.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="48" height="57"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <image width="48" height="61" source="../../../Desktop/profilD.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="2" width="42" height="57"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <image width="48" height="60" source="../../../Desktop/ProfilG.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="41" height="57"/>
+  </objectgroup>
+ </tile>
+</tileset>
