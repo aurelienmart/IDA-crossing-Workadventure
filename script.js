130f9a8ec36ded4363872bf6df020bce295dcd9a
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-06-14 09:30:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 WA.sendChatMessage('Bienvenue au 17eme étage, pensez à autoriser les pop-ups depuis ce site et à activer le son afin de profiter de toutes les fonctionnalités.','La team IDA Crossing');
-let testpopup;
 
-testpopup = WA.onEnterZone("myZone",()=>{
+
+WA.onEnterZone("myZone",()=>{
 	WA.openPopup("popup1","zone en travaux, revenez plus tard",[{
 		label:'Close'
 		className:"primary",
