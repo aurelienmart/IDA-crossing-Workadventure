6cd99c80dfdd6daee2e935c2798350eedd88f6e4
aurelienmart <85502337+aurelienmart@users.noreply.github.com>
2021-06-14 13:16:27 +0000

    Deploying to gh-pages from master @ 893f5af8c1a38150d5fa8eefb4e93bd9a23eb24c 🚀

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ WA.onEnterZone("meeting",()=>{
 	WA.openTab('https://teams.microsoft.com/l/meetup-join/19:meeting_NjBmY2EwMjQtNDkxZC00NmM4LWE3MWMtMmY4ZDZlNWVkMjk3@thread.v2/0?context=%7B%22Tid%22:%225de96c96-c87c-4dce-aad9-f5c557b52ac1%22,%22Oid%22:%2288d5518e-74f0-4910-8e9c-052164f7d7fd%22%7D')
 })
 
-let helloWorldPopup;
+let travaux;
 
 // Open the popup when we enter a given zone
 travaux= WA.onEnterZone('myZone', () => {
