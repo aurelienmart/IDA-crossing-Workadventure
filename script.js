18dabfee47e4e2b38e038fb9ca80d08f35713b90
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-06-14 15:21:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ WA.onEnterZone("meeting",()=>{
 	WA.openTab('https://teams.microsoft.com/l/meetup-join/19:meeting_NjBmY2EwMjQtNDkxZC00NmM4LWE3MWMtMmY4ZDZlNWVkMjk3@thread.v2/0?context=%7B%22Tid%22:%225de96c96-c87c-4dce-aad9-f5c557b52ac1%22,%22Oid%22:%2288d5518e-74f0-4910-8e9c-052164f7d7fd%22%7D')
 })
 
-travaux= WA.onEnterZone('myZone', () => {
+WA.onEnterZone('myZone', () => {
     WA.openPopup("popup1", 'Cette zone est en travaux, revenez plus tard', [{
         label: "Close",
         className: "primary",
@@ -18,5 +18,5 @@ travaux= WA.onEnterZone('myZone', () => {
     }]);
 });
 WA.onLeaveZone('myZone', () => {
-    travaux.close();
+    popup.close();
 });
