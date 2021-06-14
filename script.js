aaca8cfd72c814dca89b5e1b7d7f6318707faa01
aurelienmart <85502337+aurelienmart@users.noreply.github.com>
2021-06-14 07:25:18 +0000

    Deploying to gh-pages from master @ 17b2b3052ccf9bb3843c9a29d2b7dabac1ae2763 🚀

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 WA.sendChatMessage('Bienvenue au 17eme étage, pensez à autoriser les pop-ups depuis ce site et à activer le son afin de profiter de toutes les fonctionnalités.','La team IDA Crossing');
-WA.onEnterZone("myZone",()=>{
-	WA.sendChatMessage('zone en travaux revenez plus tard',"chantier")
+let testpopup;
+
+testpopup = WA.onEnterZone("myZone",()=>{
+	WA.openPopup("popup1","zone en travaux, revenez plus tard",[{
+		label:'Close'
+		className:"primary",
+		callback: (popup)=> {
+			popup.close();
+		}
+	}])
 })
