cb9d63d8f6b54161d968a8c10d3cbc40d3774a17
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-06-14 09:14:33 +0200

    test zone

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 WA.sendChatMessage('Bienvenue au 17eme étage, pensez à autoriser les pop-ups depuis ce site et à activer le son afin de profiter de toutes les fonctionnalités.','La team IDA Crossing');
+WA.onEnterZone("myZone",()=>{
+	WA.sendChatMessage('zone en travaux revenez plus tard',"chantier")
+})
