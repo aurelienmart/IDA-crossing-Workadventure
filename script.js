11f218d53cd67cc6ae068d75a9e6f443df5dbc95
aurelienmart <85502337+aurelienmart@users.noreply.github.com>
2021-06-15 08:18:35 +0000

    Deploying to gh-pages from master @ 0856354f8599331a85df808403727474f11a7cf9 🚀

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ WA.onEnterZone("meeting",()=>{
 	WA.openTab('https://teams.microsoft.com/l/meetup-join/19:meeting_NjBmY2EwMjQtNDkxZC00NmM4LWE3MWMtMmY4ZDZlNWVkMjk3@thread.v2/0?context=%7B%22Tid%22:%225de96c96-c87c-4dce-aad9-f5c557b52ac1%22,%22Oid%22:%2288d5518e-74f0-4910-8e9c-052164f7d7fd%22%7D')
 })
 
-let popup
-popup = WA.onEnterZone('myZone', () => {
-    WA.openPopup("popup1", 'Cette zone est en travaux, revenez plus tard', [{
+let travaux= null;
+
+WA.onEnterZone('myZone', () => {
+    travaux = WA.openPopup("popup1", 'Cette zone est en travaux, revenez plus tard', [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
@@ -16,6 +17,6 @@ popup = WA.onEnterZone('myZone', () => {
     }]);
 });
 
-WA.onLeaveZone('myZone', () => {
-    popup.close();
+WA.onLeaveZone('myZone',() =>{
+	travaux.close();
 });
