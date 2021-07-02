254a6b08d0460ca75ee5488b7e4b1bc3d0d10f67
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-07-02 12:01:55 +0200

    entry

--- tilesets/fantasy.tsx
+++ tilesets/fantasy.tsx
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="fantasy" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <transformations hflip="0" vflip="1" rotate="0" preferuntransformed="0"/>
  <image source="fantasy.png" width="256" height="4256"/>
  <tile id="8">
   <properties>
