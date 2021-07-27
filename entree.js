d47bcd7fbb427ca5eee3dbf98afe603168bddf05
aurelienmart <85502337+aurelienmart@users.noreply.github.com>
2021-07-27 12:05:07 +0200

    reinitialize

--- entree.js
+++ entree.js
@@ -44,8 +44,34 @@ WA.onLeaveZone('miparcours',() =>{
 	popintro2.close();
 });
 
+WA.onEnterZone('tuto', () => {
+    WA.disablePlayerControls();
+    pasc1 = WA.openPopup("Pasc", "Alors ?! C'est ta premère fois sur WorkAdventure ?", [{
+        label: "Oui",
+        className: "primary",
+        callback: (popup) => {
+            popup.close();
+            WA.openPopup("Pasc","Bienvenue à toi nouvel aventurier, tu dois avoir plein de questions",[{
+                label:"close",
+                className:"primary",
+                callback: (popup) => {
+                    popup.close();
+                    WA.restorePlayerControls()}
+            }])
+        }
+    },{label:"Non",
+        className:"primary",
+        callback: (popup)=>{
+            popup.close();
+            WA.openPopup("Pasc","Tu connais le chemin alors, je te laisse repartir",[{
+                label:"close",
+                className:"primary",
+                callback: (popup) => {
+                    popup.close();
+                    WA.restorePlayerControls()}
+            }])
+            
+        }
 
-console.log('dehors')
-$.getScript('./dialogcreator.js', function(){
- console.log('ici ça marche');
-});
+    }]);
+})
