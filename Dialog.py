abae11bd45b162b9ea7964f23ff05f985502a5e0
aurelienmart <85502337+aurelienmart@users.noreply.github.com>
2021-07-28 11:34:07 +0000

    Deploying to gh-pages from master @ 3bfff990d054b6bf57a6ad22a4deea13e36cccb7 🚀

---
+++ Dialog.py
@@ -0,0 +1,36 @@
+
+#creer une fonction CSV => dico
+#creer une fonction recursive de génération du texte
+#creer une fonction de génération du nouveau fichier et sauvegarde de l"ancienne version
+#créer une fonction de restauration à la version antèrieure
+
+
+#arguments (excel, fichier js, freeze, zone, target)
+
+
+#optionnel: interface graphique tkinter
+import csv
+
+def to_dico(excel): #Prend le nom du fichier csv en entree et renvoie un dictionnaire organisée par NodeID puis par elements (Text, buttons, childs)
+    dico={}
+    with open(excel,newline="") as csvfile:
+        reader=csv.DictReader(csvfile)
+        for row in reader:
+            dico[row["NodeID"]]={"text":row["Text"],"buttons":row["Buttons"].split(","),"childs":row["Childs"].split(",")}
+    return dico
+
+
+dico=to_dico("dialog.csv")
+def recursPopup(line,target):
+    if len(line["buttons"])==1 and line["childs"][0]=="":
+        return "WA.openPopup('"+target+"','"+line["text"]+"',[{label:'"+line["buttons"][0]+"',className:'primary',callback:(popup)=>{popup.close();WA.restorePlayerControls()}}])"
+
+
+    else:
+        str="WA.openPopup('"+target+"','"+line["text"]+"',["
+        for i in range(len(line['buttons'])):
+            str+="{label:'"+line['buttons'][0]+"',className:'primary',callback:(popup)=>{popup.close();"+recursPopup(dico[line['childs'][i]],target)+"}},"
+        str+="])"
+        return str
+        
+print(recursPopup(dico['0'],'Pasc'))
--- entree.js
+++ entree.js
@@ -45,33 +45,5 @@ WA.onLeaveZone('miparcours',() =>{
 });
 
 WA.onEnterZone('tuto', () => {
-    WA.disablePlayerControls();
-    pasc1 = WA.openPopup("Pasc", "Alors ?! C'est ta premère fois sur WorkAdventure ?", [{
-        label: "Oui",
-        className: "primary",
-        callback: (popup) => {
-            popup.close();
-            WA.openPopup("Pasc","Bienvenue à toi nouvel aventurier, tu dois avoir plein de questions",[{
-                label:"close",
-                className:"primary",
-                callback: (popup) => {
-                    popup.close();
-                    WA.restorePlayerControls()}
-            }])
-        }
-    },{label:"Non",
-        className:"primary",
-        callback: (popup)=>{
-            popup.close();
-            WA.openPopup("Pasc","Tu connais le chemin alors, je te laisse repartir",[{
-                label:"close",
-                className:"primary",
-                callback: (popup) => {
-                    popup.close();
-                    WA.restorePlayerControls()}
-            }])
-            
-        }
-
-    }]);
+    WA.openPopup('Pasc','�a va ?',[{label:'Oui',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Super',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.restorePlayerControls()}}])}},{label:'Oui',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Mince, il tarrive quoi ?',[{label:'Fatigu�',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Va faire une sieste',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.restorePlayerControls()}}])}},{label:'Fatigu�',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Tas raison',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.restorePlayerControls()}}])}},])}},])
 })
