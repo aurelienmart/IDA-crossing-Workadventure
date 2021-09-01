edd4489b5a34008ed6020aa4293324074a11edf2
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-09-01 15:16:17 +0200

    elvator song

--- scripts/elevator.js
+++ scripts/elevator.js
@@ -1,9 +1,22 @@
+var = mySound = WA.sound.loadSound("../medias/elevator.mp3");
+var config={
+    volume : 0.5,
+    loop: true,
+    rate : 1,
+    detune: 1,
+    delay : 0,
+    seek: 0,
+    mute : false
+}
+
 WA.onEnterZone('start', () => {
+    mySound.play(config)
     WA.disablePlayerControls();
     popintro2 = WA.openPopup("target", 'A quel étage veux-tu aller ?', [{
         label: "17eme",
         className: "primary",
         callback: (popup) => {
+            mySound.stop();
             WA.nav.goToRoom('../maps/17.json')
         }
     },
@@ -11,6 +24,7 @@ WA.onEnterZone('start', () => {
         label: "21eme",
         className: "primary",
         callback: (popup) => {
+            mySound.stop();
             WA.nav.goToRoom('../maps/21.json')
         }
     },
@@ -18,6 +32,7 @@ WA.onEnterZone('start', () => {
         label: "Au bureau RH",
         className: "primary",
         callback: (popup) => {
+            mySound.stop();
             WA.nav.goToRoom('../maps/RH.json')
         }
     },
@@ -25,6 +40,7 @@ WA.onEnterZone('start', () => {
         label: "Ressources",
         className: "primary",
         callback: (popup) => {
+            mySound.stop();
             WA.nav.goToRoom('../maps/ressources.json')
         }
     },
@@ -32,6 +48,7 @@ WA.onEnterZone('start', () => {
         label: "Dehors",
         className: "primary",
         callback: (popup) => {
+            mySound.stop();
             WA.nav.goToRoom('../maps/entree.json')
         }
     }
