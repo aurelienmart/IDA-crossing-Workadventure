644aed2c10d5077046ed4b57789162eaf14389b8
aurelienmart <85502337+aurelienmart@users.noreply.github.com>
2021-09-01 12:51:48 +0000

    Deploying to gh-pages from master @ 116140b3c657aa4783b513be2da66dae14801bad 🚀

---
+++ scripts/elevator.js
@@ -0,0 +1,45 @@
+WA.onEnterZone('start', () => {
+    popintro2 = WA.openPopup("target", 'A quel étage veux-tu aller ?', [{
+        label: "17eme",
+        className: "primary",
+        callback: (popup) => {
+            WA.nav.goToRoom('../maps/17.json')
+        }
+    },
+    {
+        label: "21eme",
+        className: "primary",
+        callback: (popup) => {
+            WA.nav.goToRoom('../maps/21.json')
+        }
+    },
+    {
+        label: "Au bureau RH",
+        className: "primary",
+        callback: (popup) => {
+            WA.nav.goToRoom('../maps/RH.json')
+        }
+    },
+    {
+        label: "Ressources",
+        className: "primary",
+        callback: (popup) => {
+            WA.nav.goToRoom('../maps/ressources.json')
+        }
+    },
+    {
+        label: "Dehors",
+        className: "primary",
+        callback: (popup) => {
+            WA.nav.goToRoom('../maps/entree.json')
+        }
+    }
+
+
+
+
+
+
+
+]);
+});
