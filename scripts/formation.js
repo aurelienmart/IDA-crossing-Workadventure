49fb8460eb0de8f7900e52ab1788db1d0ac617aa
aurelienmart <85502337+aurelienmart@users.noreply.github.com>
2021-09-20 08:40:12 +0000

    Deploying to gh-pages from master @ e908e71f57f8d392d4448ddfacd098d8a5338ee4 🚀

---
+++ scripts/formation.js
@@ -0,0 +1,13 @@
+WA.room.onEnterZone('obj1', () => {
+    obj1 = WA.ui.openPopup("target1", "Les objectifs de la semaine 1 sont:</br> Découvrir le parcours d'intégration <br> Connaître les bonnes pratiques en télétravail <br> Se réapproprier les basiques", [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+WA.onLeaveZone('obj1',() =>{
+	obj1.close();
+});
