70bd1e6f57dff6a71651cea6ed26fa24fe226c9c
aurelienmart <85502337+aurelienmart@users.noreply.github.com>
2021-09-20 08:57:18 +0000

    Deploying to gh-pages from master @ c97e3a51a69b95298b965abea48eed353456224c 🚀

--- scripts/formation.js
+++ scripts/formation.js
@@ -1,3 +1,6 @@
+
+
+////////////Semaine 1
 WA.room.onEnterZone('obj1', () => {
     obj1 = WA.ui.openPopup("target1", "Les objectifs de la semaine 1 sont:\n /Découvrir le parcours d'intégration \n /Connaître les bonnes pratiques en télétravail \n /Se réapproprier les basiques", [{
         label: "Close",
@@ -9,5 +12,74 @@ WA.room.onEnterZone('obj1', () => {
     }]);
 });
 WA.onLeaveZone('obj1',() =>{
+	obj1.close();
+});
+
+WA.room.onEnterZone('cal1', () => {
+    cal1 = WA.ui.openPopup("target1", "Il n'y a pas de RDV cette semaine", [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+WA.onLeaveZone('obj1',() =>{
+	cal1.close();
+});
+
+WA.room.onEnterZone('res1', () => {
+    obj1 = WA.ui.openPopup("target1", "Voici les différentes ressources de la semaine 1", [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    },
+    {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    },
+    {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    },
+    {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    },
+    {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    },
+    {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }
+]);
+});
+WA.onLeaveZone('res1',() =>{
 	obj1.close();
 });
