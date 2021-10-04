0633af795583be58fa52047d9d341e645761e6e9
Aurélien Martinez <martinez.aurelien@hotmail.fr>
2021-10-04 14:51:21 +0200

    API V2 entrée,RH

--- scripts/entree.js
+++ scripts/entree.js
@@ -1,6 +1,6 @@
 
-WA.onEnterZone('start', () => {
-    popintro1 = WA.openPopup("start 1", 'Bienvenue sur Workaventure,pensez à autoriser les pop-ups depuis ce site et à activer le son afin de profiter de toutes les fonctionnalités.', [{
+WA.room.onEnterZone('start', () => {
+    popintro1 = WA.ui.openPopup("start 1", 'Bienvenue sur Workaventure,pensez à autoriser les pop-ups depuis ce site et à activer le son afin de profiter de toutes les fonctionnalités.', [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
@@ -10,12 +10,12 @@ WA.onEnterZone('start', () => {
     }]);
 });
 
-WA.onLeaveZone('start',() =>{
+WA.room.onLeaveZone('start',() =>{
 	popintro1.close();
 });
 
-WA.onEnterZone('entretour', () => {
-    pasc1 = WA.openPopup("Pasc", "C'est une heure pour arriver au bureau ça ?! Allez on se dépêche !!", [{
+WA.room.onEnterZone('entretour', () => {
+    pasc1 = WA.ui.openPopup("Pasc", "C'est une heure pour arriver au bureau ça ?! Allez on se dépêche !!", [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
@@ -25,12 +25,12 @@ WA.onEnterZone('entretour', () => {
     }]);
 });
 
-WA.onLeaveZone('entretour',() =>{
+WA.room.onLeaveZone('entretour',() =>{
 	pasc1.close();
 });
 
-WA.onEnterZone('miparcours', () => {
-    popintro2 = WA.openPopup("start2", "Si tu es nouveau ici n'hésite pas à aller voir Pascal, au pied de la tour pour plus d'informations", [{
+WA.room.onEnterZone('miparcours', () => {
+    popintro2 = WA.ui.openPopup("start2", "Si tu es nouveau ici n'hésite pas à aller voir Pascal, au pied de la tour pour plus d'informations", [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
@@ -40,7 +40,7 @@ WA.onEnterZone('miparcours', () => {
     }]);
 });
 
-WA.onLeaveZone('miparcours',() =>{
+WA.room.onLeaveZone('miparcours',() =>{
 	popintro2.close();
 });
-WA.onEnterZone('tuto',()=>{WA.disablePlayerControls();WA.openPopup('Pasc','Bonjour et bienvenue sur Workadventure !',[{label:'Où suis-je ?',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Tu es dans une simulation des bureaux de Wavestone de Paris',[{label:'Suite',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Tu peux discuter avec tes collègues grace à ton micro et ta caméra ou à  travers du chat !',[{label:'Suite',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Accède à waveplace depuis les ordinateurs du 17ème étage ou bien rejoins tes amis à la machine à café !',[{label:'Suite',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Workadventure est encore en chantier, certains étages ne sont pas encore finis mais l’équipe IDA Crossing y travaille d’arrache-pied !',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.restorePlayerControls()}}])}},])}},])}},])}},{label:' Je connais !',className:'primary',callback:(popup)=>{popup.close();WA.openPopup('Pasc','Je te laisse alors, à bientôt !',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.restorePlayerControls()}}])}},]);})
+WA.room.onEnterZone('tuto',()=>{WA.controls.disablePlayerControls();WA.ui.openPopup('Pasc','Bonjour et bienvenue sur Workadventure !',[{label:'Où suis-je ?',className:'primary',callback:(popup)=>{popup.close();WA.ui.openPopup('Pasc','Tu es dans une simulation des bureaux de Wavestone de Paris',[{label:'Suite',className:'primary',callback:(popup)=>{popup.close();WA.ui.openPopup('Pasc','Tu peux discuter avec tes collègues grace à ton micro et ta caméra ou à  travers du chat !',[{label:'Suite',className:'primary',callback:(popup)=>{popup.close();WA.ui.openPopup('Pasc','Accède à waveplace depuis les ordinateurs du 17ème étage ou bien rejoins tes amis à la machine à café !',[{label:'Suite',className:'primary',callback:(popup)=>{popup.close();WA.ui.openPopup('Pasc','Workadventure est encore en chantier, certains étages ne sont pas encore finis mais l’équipe IDA Crossing y travaille d’arrache-pied !',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.controls.restorePlayerControls()}}])}},])}},])}},])}},{label:' Je connais !',className:'primary',callback:(popup)=>{popup.close();WA.ui.openPopup('Pasc','Je te laisse alors, à bientôt !',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.controls.restorePlayerControls()}}])}},]);})
