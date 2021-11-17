b50232f767ee1ff5066f939130a4713efe425999
AurelienmartW <92736328+AurelienmartW@users.noreply.github.com>
2021-11-17 15:48:52 +0000

    Deploying to gh-pages from master @ 8e7882caeb39cbeff4228c12a4a05926bea35c47 🚀

--- maps/tilesets/carpet-34x34.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="carpet-34x34" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="carpet-34x34.png" width="256" height="256"/>
-</tileset>
--- maps/tilesets/custom_ws_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="custom_ws_32x32" tilewidth="32" tileheight="32" tilecount="24" columns="6">
- <image source="custom_ws_32x32.png" width="192" height="128"/>
-</tileset>
