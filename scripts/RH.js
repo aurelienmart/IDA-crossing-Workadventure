2f1438ff3a2363606708423f2bb6058cc11e554f
aurelienmart <aurelien.martinez@wavestone.com>
2022-01-04 16:40:18 +0100

    fix bug definition popup

--- scripts/RH.js
+++ scripts/RH.js
@@ -1,5 +1,6 @@
 WA.room.onEnterZone('start',()=>{WA.controls.disablePlayerControls();WA.ui.openPopup('target','Bonjour et bienvenue au bureau RH d’IDA',[{label:'C’est quoi ?',className:'primary',callback:(popup)=>{popup.close();WA.ui.openPopup('target','Je suis Océane DHOME, Je m’occupe des tâches suivantes: \n -Pilotage Rh de la practice en lien avec le Steering Commitee \n -Suivi et accompagnement des équipes et du management \n -Gestion des mobilités internes',[{label:'Suite',className:'primary',callback:(popup)=>{popup.close();WA.ui.openPopup('target','Pour rencontrer les Référentes RH tu peux aller au bench en haut à gauche. \nSi tu veux discuter avec les Assitantes RH tu les trouveras au bench en haut à droite. \nEt finalement si tu as des questions sur la gestion du personnel et des fiches de paie tu peux aller au dernier bench en bas à droite.',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.controls.restorePlayerControls()}}])}},])}},{label:' Je connais !',className:'primary',callback:(popup)=>{popup.close();WA.ui.openPopup('target','Je te laisse alors, à bientôt !',[{label:'Close',className:'primary',callback:(popup)=>{popup.close();WA.controls.restorePlayerControls()}}])}},]);})
 
+let RRH=null;
 WA.room.onEnterZone('RRH', () => {
     RRH = WA.ui.openPopup("target", "Bonjour nous sommes les référentes RH,\n nous sommes en charge des éléments suivants: \n \n -Suivi et accompagnement des collaborateurs sur les sujets d'évolution, parcours, mobilité interne... \n-Appui au management de la practice sur les sujets RH \n -Animation des processus et politiques RH sur le périmètre \n - Intégration des nouveaux collaborateurs \n -Suivi des periodes d'éssai, stages et alternances \n -Animation et com' interne", [{
         label: "Contacter Anne-Lise",
@@ -38,6 +39,7 @@ WA.room.onLeaveZone('RRH',() =>{
 	RRH.close();
 });
 
+let ARH=null;
 WA.room.onEnterZone('ARH', () => {
     ARH = WA.ui.openPopup("target", "Bonjour nous sommes les Assitantes RH, \n \n\n Lucie est en charge des éléments suivants: \n -Gestion des imputations \n -Suivi et déclaration des astreintes et intervention en heures non ouvrées \n \n Charlène s'occupe des sujets suivants: \n -Organisation des formations  \n -Intégration des nouveaux collaborateurs \n -Réalisation des démarches pour les collaborateurs de nationalité étrangère \n \n Sophie est responsable des éléments suivants: \n -Rédaction des attestations employeur \n -Distribution des tickets restaurant \n -Rédaction des Ordres de Mission", [{
         label: "Contacter Lucie",
@@ -76,6 +78,7 @@ WA.room.onLeaveZone('ARH',() =>{
 	ARH.close();
 });
 
+let payroll=null;
 WA.room.onEnterZone('payroll', () => {
     payroll = WA.ui.openPopup("target", "Si tu as des questions sur les sujets suivants:\n -Information sur les fiches de paie \n -Gestion des avances sur frais \n -Mise à jour des données personnelles \n -Suivi des arrêts maladie et gestion des dossiers Mutuelle et Prévoyance \n -Traitement des notes de frais non refacturables \n -Remboursement des abonnements de transports en commun \n -Suivi des absences (congés, CDT, sans solde) \n tu peux contacter la Mailing list IDA Payroll and staff administration", [{
         label: "Envoyer un mail",
