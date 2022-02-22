c9766c682fbf1b42c0ae741af77645e3a96c6719
AurelienmartW <92736328+AurelienmartW@users.noreply.github.com>
2022-02-22 09:13:06 +0000

    Deploying to gh-pages from master @ 3562675c2d492b5d6115b964f9290df7e88ef9c6 🚀

--- scripts/C.js
+++ scripts/C.js
@@ -1,14 +1,12 @@
 let guide= null;
 
 WA.room.onEnterZone('start', () => {
-    WA.controls.disablePlayerControls();
     guide = WA.ui.openPopup("target", 'Choisissez la bonne porte si vous espérez trouver la sortie !!', [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
             // Close the popup when the "Close" button is pressed.
             popup.close();
-            WA.controls.restorePlayerControls();
         }
     }]);
 });
