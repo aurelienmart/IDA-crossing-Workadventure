6d2dcbdf384554add373c81c813cae3249c8ee1d
ClaraHUY <clara.huyghe@wavestone.com>
2022-03-16 11:40:06 +0100

    A1 - Lien et description

--- scripts/GG/A/A1.js
+++ scripts/GG/A/A1.js
@@ -1,6 +1,8 @@
 let popintro1=null;
 WA.room.onEnterZone('intro', () => {
-    popintro1 = WA.ui.openPopup("presentation", 'Insérer texte', [{
+    popintro1 = WA.ui.openPopup("presentation", ' Les IDA Live, c’est le rendez-vous régulier de la practice IDA pour transmettre à l'ensemble à tout le monde les dernières actualités, décisions management et évènements à venir.
+
+Ne loupez pas nos différents formats : Live show, JT, émission d'investigation, Jeux TV… Pour tout savoir des sujets brûlants de la practice ', [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
@@ -19,7 +21,7 @@ WA.room.onEnterZone('teams', () => {
     meeting=WA.ui.displayActionMessage({
         message: "Appuyez sur Espace pour accéder à la réunion teams",
         callback: () => {
-            WA.nav.openTab("insérer lien")
+            WA.nav.openTab("https://teams.microsoft.com/l/meetup-join/19%3ameeting_OGJkOWJiOTktNzhiMC00YTFlLWFlNmItZDY3NjYwMTJhNWM2%40thread.v2/0?context=%7b%22Tid%22%3a%225de96c96-c87c-4dce-aad9-f5c557b52ac1%22%2c%22Oid%22%3a%2290ab68a1-0b52-40b6-a50f-1d444e6122a5%22%7d")
         }
     })
 });
