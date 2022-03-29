ecb584e5a8357d89830521b14fb9811ec132745c
thomas.gallois <thomas.gallois@wavestone.com>
2022-03-29 14:57:08 +0200

    Orchestre de face et correction texte Pascal

--- scripts/GG/lobbyGG.js
+++ scripts/GG/lobbyGG.js
@@ -4,7 +4,7 @@ WA.room.onEnterZone('Pasc', () => {
     PascA=WA.ui.displayActionMessage({
         message: "Parler à Pascal",
         callback: () => {
-            Pasc=WA.ui.openPopup('target',"KingPasc : 'Bienvenue aux Golden Globes IDA, la cérémonie qui célèbre les initiatives IDA. Partez à la rencontre des acteurs qui ont travaillé sur ces projets pour découvrir les coulisses et poser toutes vos questions !'",
+            Pasc=WA.ui.openPopup('target',"KingPasc : Bienvenue aux Golden Globes IDA, la cérémonie qui célèbre les initiatives IDA. Partez à la rencontre des acteurs qui ont travaillé sur ces projets pour découvrir les coulisses et poser toutes vos questions !",
             [{label:'Close',className:'primary',callback:(popup)=>
             {popup.close();}}])}
     })
