697a92633ccbda4c86fee16649486d80ec890640
aurelienmart <aurelien.martinez@wavestone.com>
2022-06-23 14:49:04 +0200

    add collides and Yves jokes

--- scripts/cercles/nextgenIT.js
+++ scripts/cercles/nextgenIT.js
@@ -128,3 +128,60 @@ WA.room.onEnterZone('Robot', () => {
 WA.room.onLeaveZone('Robot',() =>{
     popRobot.close();
 });
+
+//Zone de Yves
+let jokes ={1:{'joke':"Que se passe t’il quand deux poissons s’énervent ?",'sol':'Le thon monte'},
+            2:{'joke':"Quel est le comble pour un plombier ?",'sol':'Que ses clients prennent la fuite !'},
+            3:{'joke':"Pourquoi les mamies sont-elles très efficaces pour faire le ménage ?",'sol':'Car elles ont 80 balais.'},
+            4:{'joke':"Comment appelle t’on un lion qui se rebelle ?",'sol':'Une rébellion'},
+            5:{'joke':"Que dit Frodon devant sa maison ?",'sol':'C’est là que j’hobbit…'},
+            6:{'joke':"Comment appelle-t-on une chauve-souris qui a des cheveux ?",'sol':'Une souris'},
+            7:{'joke':"Pourquoi Harry Potter chuchotte ?",'sol':' Parce que Dumbledore…'},
+            8:{'joke':"J’ai raconté une blague à un Parisien…, tu sais ce qu'il a fait ?",'sol':'Il a pas ri…'},
+            9:{'joke':"Est-ce que faire des gâteaux d’instinct C ‘est avoir la pate à tarte innée ?"},
+            10:{'joke':"C’est l’histoire d’un pingouin qui respire par les fesses Un jour il s’assoit et il meurt."},
+            11:{'joke':"Quel fruit le poisson déteste-il le plus ?",'sol':'La pêche.'},
+            12:{'joke':"Pourquoi les citrouilles sont heureuses ?",'sol':'Car où qu’elles aillent, leurs potes iront !'},
+            13:{'joke':"Comment on appelle une bagarre entre une mouche et un escargot ?",'sol':'Une escarmouche'},
+            14:{'joke':"Que crie un donut sur la plage ?",'sol':'Hey, je vais me beignet !!!'},
+            15:{'joke':"Quelle est la couleur du virus de la grippe ?",'sol':'Gris pâle'},
+            16:{'joke':"Qu’est-ce que la théière dit à l’ascenseur ?",'sol':'« Je veux monter »'},
+            17:{'joke':"Qu’est-ce qu’on dit d’une chaussure curieuse ?",'sol':'Qu’elle se mêle de tout'},
+            18:{'joke':"Pourquoi les marchands de savon font fortune ?",'sol':'Car leurs clients les savent honnêtes !'},
+            19:{'joke':"C’est quoi une pomme dauphine ?",'sol':'C’est celle qui a fini 2eme à Miss patate'},
+            20:{'joke':"Que font les dinosaures quand ils n’arrivent pas à se mettre d’accord ?",'sol':'Un tirajosor…'},
+            }
+
+WA.room.onEnterZone('Yves', () => {
+    exitA=WA.ui.displayActionMessage({
+        message: "Parler à Yves",
+        callback: () => {
+        WA.controls.disablePlayerControls()
+        x= Math.floor(Math.random()*Object.keys(jokes).length)+1
+        let button=''
+            if ('sol' in jokes[x]){
+                button="Je ne sais pas"
+            }
+            else{
+                button="close"
+            }
+        WA.ui.openPopup("targetBas", jokes[x]['joke'], [{
+                    label: button,
+                    className: "primary",
+                    callback: (popup) => {
+                        popup.close()
+                        if(button=="Je ne sais pas"){
+                        WA.ui.openPopup("targetBas", jokes[x]['sol'], [{
+                            label: "Close",
+                            className: "primary",
+                            callback: (popup) => {
+                                popup.close()
+                                WA.controls.restorePlayerControls()
+                            }
+                        }])}
+                        else{
+                            WA.controls.restorePlayerControls()
+                        };
+                    }
+                }]);
+            ;}})})
